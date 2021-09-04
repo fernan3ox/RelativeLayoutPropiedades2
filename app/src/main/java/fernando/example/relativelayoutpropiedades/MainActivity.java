552d40c8beb998642b7163ccd020ba6438432ce3
fernan3ox <fernandoramiro21@gmail.com>
2021-09-04 01:50:39 -0300

    firt commit

---
+++ app/src/main/java/fernando/example/relativelayoutpropiedades/MainActivity.java
@@ -0,0 +1,14 @@
+package fernando.example.relativelayoutpropiedades;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
